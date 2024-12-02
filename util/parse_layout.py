e8c237461b187c73b7f5fee6a12c88d8151cf23c
Guido Bartolucci <guido@bartolucci.org>
2024-12-01 19:21:01 -0500

    Checkpoint

--- util/parse_layout.py
+++ util/parse_layout.py
@@ -51,6 +51,7 @@ def split(s):
     return re.findall(r'\n?(.╭─+╮.+?╰─+╯)\w*', s, re.S)
 
 def extract_middle(s):
+    "Split apart into index, name, and list of rows"
     lines = s.splitlines()
     top, middle, bottom = lines[0], lines[1:-1], lines[-1]
     column_sizes = list(map(len, re.search(r'(.*╭)(─+)(╮.*)', top).groups()))
@@ -58,30 +59,39 @@ def extract_middle(s):
     # This splits the lines into prefix, body, suffix and then puts
     # the prefixes together, bodies togethers, and suffixes together.
     columns = [list(map(''.join, split_into(s, column_sizes))) for s in middle]
-    segments = list(transpose(columns))
+    segments = list(transpose(columns)) # transpose is awesome!
     index = ''.join([s.strip(' │') for s in segments[0]])
     name = ''.join([s.strip(' │') for s in segments[2]]) or f'layer_{i}'
     rows = [s.split() for s in segments[1]]
     return index, name, rows
 
-for i, s in enumerate(split(layers)): # for i, s in enumerate(split(combos)):
-    # 1. Split apart into index, name, and list of rows
-    index, name, rows = extract_middle(s)
-    
-    # 2. Remove rows and columns based on intended keyboard size
+def whittle_down(rows):
+    "Remove rows and columns based on intended keyboard size"
     del rows[3] # We've given up on this row of thumb keys, too many keys clanky!
     if columnslen == 5:
         rows = [r[1:-1] for r in rows[:3]] + rows[3:]
     if thumbslen == 3:
         rows = rows[:3] + [rows[3][1:-1]]
+    return rows
 
-    # 3. Replace keys in rows with keycodes or drawing config
+for i, s in enumerate(split(layers)):
+    index, name, rows = extract_middle(s)
+    rows = whittle_down(rows)
     if outtype == 'firmware':
         rows = [[keycodes[k] for k in r] for r in rows]
         maxlen = min(12, max([max(map(len, r)) for r in rows]))
         rows = [[k.center(maxlen) for k in r] for r in rows]
         layer = '\n '.join(map(' '.join, rows))
         print(f"ZMK_LAYER({name},\n {layer})")
+    elif outtype == 'drawing':
+        config['layers'][name] = [[drawingcodes.get(k, k) for k in r] for r in rows]
+
+for i, s in enumerate(split(combos)):
+    index, name, rows = extract_middle(s)
+    rows = whittle_down(rows)
+
+    if outtype == 'firmware':
+        pass
     elif outtype == 'drawing':
         name, separate = re.match(r'(\w+)(\*?)', name).groups()
         keysdown = [i for i, k in enumerate(flatten(rows)) if k == '']
@@ -92,7 +102,6 @@ for i, s in enumerate(split(layers)): # for i, s in enumerate(split(combos)):
             'draw_separate': bool(separate),
         }
         config['combos'].append(d)
-        config['layers'][name] = [[drawingcodes.get(k, k) for k in r] for r in rows]
 
 if outtype == 'drawing':
     print(yaml.dump(config, default_flow_style=None, sort_keys=False))
